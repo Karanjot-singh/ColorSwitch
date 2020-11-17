0e3c1be04b08b2bcf9f2a8a205d504419fb41b6f
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-11-17 17:56:58 +0530

    Added gameplay Pane & changed code structure

--- src/sample/GameplayController.java
+++ src/sample/GameplayController.java
@@ -1,12 +1,16 @@
 package sample;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class GameplayController {
 
     public Button backButton;
     public Label gameMsg;
-
     public void backClicked()
     {
         Main.window.setScene(Main.home);
