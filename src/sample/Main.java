d6400d3b91d807ed26f723ac9eedffc674083a1d
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-11-22 22:51:11 +0530

    Directory structure modified

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,73 +3,70 @@ package sample;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.*;
+import javafx.util.Duration;
 
-public class Main extends Application
-{
-	static Stage window;
-	static Scene homeScene, gameplayScene, loadGameScene, helpScene, settingsScene, pausePopupScene, closePopupScene;
+import java.nio.file.Paths;
 
-	//static HBox gameplayPane;
+public class Main extends Application {
+    static Stage window;
+    static Scene homeScene, gameplayScene, loadGameScene, helpScene, settingsScene, pausePopupScene, closePopupScene;
+    static MediaPlayer mediaPlayer;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception
-	{
-		window = primaryStage;
-
-		Parent homeRoot = FXMLLoader.load(getClass().getResource("home.fxml"));
-		Parent gameplayRoot = FXMLLoader.load(getClass().getResource("gameplay.fxml"));
-		Parent loadGameRoot = FXMLLoader.load(getClass().getResource("loadGame.fxml"));
-		Parent helpRoot = FXMLLoader.load(getClass().getResource("help.fxml"));
-		Parent settingsRoot = FXMLLoader.load(getClass().getResource("settings.fxml"));
-		Parent pausePopupRoot = FXMLLoader.load(getClass().getResource("pausePopup.fxml"));
-		Parent closePopupRoot = FXMLLoader.load(getClass().getResource("closePopup.fxml"));
-
-
-		//new Game();
-		//gameplayPane = new HBox(30);
-
-
-		homeScene = new Scene(homeRoot); //, 600, 300
-		gameplayScene = new Scene(gameplayRoot);
-		loadGameScene = new Scene(loadGameRoot);
-		helpScene = new Scene(helpRoot);
-		settingsScene = new Scene(settingsRoot);
-		closePopupScene = new Scene(closePopupRoot);
-		pausePopupScene = new Scene(pausePopupRoot);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        Parent homeRoot = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Parent gameplayRoot = FXMLLoader.load(getClass().getResource("gameplay.fxml"));
+        Parent loadGameRoot = FXMLLoader.load(getClass().getResource("loadGame.fxml"));
+        Parent helpRoot = FXMLLoader.load(getClass().getResource("help.fxml"));
+        Parent settingsRoot = FXMLLoader.load(getClass().getResource("settings.fxml"));
+        Parent pausePopupRoot = FXMLLoader.load(getClass().getResource("pausePopup.fxml"));
+        Parent closePopupRoot = FXMLLoader.load(getClass().getResource("closePopup.fxml"));
+        startGameMusic();
+        homeScene = new Scene(homeRoot); //, 600, 300
+        gameplayScene = new Scene(gameplayRoot);
+        loadGameScene = new Scene(loadGameRoot);
+        helpScene = new Scene(helpRoot);
+        settingsScene = new Scene(settingsRoot);
+        closePopupScene = new Scene(closePopupRoot);
+        pausePopupScene = new Scene(pausePopupRoot);
 
 //		homeScene.getStylesheets().add(getClass().getResource("generalStyle.css").toExternalForm());
+        window.setOnCloseRequest(e -> {
+            e.consume();
+            closeProgram();
+        });
+        startGameMusic();
+        window.setTitle("Color Switch");
+        window.setScene(homeScene);
+        window.centerOnScreen();
+        window.show();
+    }
 
-		//Scene gameScreen = new Scene(gameplayPane,200,300,Color.BLACK);
-		//primaryStage.setScene(gameScreen);
+    void startGameMusic() {
+        Media backgroundMusic = new Media(getClass().getResource("/assets/music.mp3").toString());
+        mediaPlayer = new MediaPlayer(backgroundMusic);
+        mediaPlayer.setAutoPlay(true);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.setStartTime(Duration.seconds(0));
+        mediaPlayer.setStopTime(Duration.seconds(183));
+        mediaPlayer.play();
+    }
 
-		window.setOnCloseRequest(e -> {
-			e.consume();
-			closeProgram();
-		});
+    static void closeProgram() {
+        Boolean ans = ClosePopupController.display();
 
-		window.setTitle("Color Switch");
-		window.setScene(homeScene);
-		window.centerOnScreen();
-		window.show();
-	}
+        if (ans) {
+            window.close();
+        }
 
-	static void closeProgram() {
-		Boolean ans = ClosePopupController.display();
-
-		if(ans)
-		{
-			window.close();
-		}
-
-	}
+    }
 
 
-	public static void main(String[] args)
-	{
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
