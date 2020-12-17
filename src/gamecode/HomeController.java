0707d58961f4d5cf743cd8404e621f3f73c8f252
Samarth Saxena <saxena.samarth02@gmail.com>
2020-12-17 23:28:35 +0530

    waste

--- src/gamecode/HomeController.java
+++ src/gamecode/HomeController.java
@@ -1,12 +1,21 @@
 package gamecode;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class HomeController {
     @FXML
@@ -52,5 +61,4 @@ public class HomeController {
         }
         PlayerInfoController.display();
     }
-
 }
--- src/gamecode/Main.java
+++ src/gamecode/Main.java
@@ -272,3 +272,4 @@ class Music implements Runnable {
         startMusic();
     }
 }
+
